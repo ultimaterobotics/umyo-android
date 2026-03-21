fbccfee4cd5e8264907b870fa34e96df8c206cab
astartav <astarta.v@gmail.com>
2026-03-21 22:54:03 +0200

    chore: bump version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.uf1bridgedemo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
